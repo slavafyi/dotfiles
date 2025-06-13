b76b83263ef22c3b2535c69c2ca2f24d0db46021
Slava Maksimov <204412653+slavafyi@users.noreply.github.com>
2025-06-13 14:55:10 +0300

    fix(configs): fish issue with start ssh-agent

--- configs/fish/.config/fish/conf.d/90-ssh.fish
+++ configs/fish/.config/fish/conf.d/90-ssh.fish
@@ -1,7 +1,15 @@
-if not set -q SSH_AUTH_SOCK
-  eval "$(ssh-agent -c)" 2>/dev/null
-  set -Ux SSH_AUTH_SOCK "$SSH_AUTH_SOCK"
-  set -Ux SSH_AGENT_PID "$SSH_AGENT_PID"
-end
+if type -q ssh-agent
+  if type -q pgrep
+    if not pgrep -u "$USER" ssh-agent > /dev/null
+      rm -v "$SSH_AGENT_SOCK"
+    end
+  end
 
-ssh-add ~/.ssh/id_ed25519 2>/dev/null
+  if not test -S SSH_AUTH_SOCK
+    eval "$(ssh-agent -c)" > /dev/null
+    set -Ux SSH_AUTH_SOCK "$SSH_AUTH_SOCK"
+    set -Ux SSH_AGENT_PID "$SSH_AGENT_PID"
+  end
+
+  ssh-add ~/.ssh/id_ed25519 2> /dev/null
+end
