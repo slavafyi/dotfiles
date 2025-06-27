d97d3b48ee72df59e34b4bed607fed02bac1d110
Slava Maksimov <204412653+slavafyi@users.noreply.github.com>
2025-06-28 00:39:16 +0300

    feat(scripts): add setup easy effects

--- scripts/arch/optional-extras.sh
+++ scripts/arch/optional-extras.sh
@@ -14,6 +14,22 @@ setup_obsidian() {
   print_in_green "Obsidian installed successfully!"
 }
 
+setup_easyeffects() {
+  print_in_purple "Setting up EasyEffects..."
+  sleep 2
+  sudo pacman -Sy --noconfirm --needed easyeffects lsp-plugins calf
+  local base_config="$HOME/.config/easyeffects"
+  local config_dir="$base_config/output"
+  local irs_dir="$base_config/irs"
+  mkdir -p "$config_dir" "$irs_dir"
+  local fw_url="https://raw.githubusercontent.com/FrameworkComputer/linux-docs/main/easy-effects"
+  curl -fo "$config_dir/fw13-easy-effects.json" "$fw_url/fw13-easy-effects.json"
+  curl -fo "$irs_dir/IR_22ms_27dB_5t_15s_0c.irs" "$fw_url/irs/IR_22ms_27dB_5t_15s_0c.irs"
+  print_in_green "EasyEffects installed successfully!"
+}
+
+
 optional_extras() {
   setup_obsidian
+  setup_easyeffects
 }
