270266283e1ecc2ab73fe49279bf56c3687219dc
Slava Maksimov <28267670+slavafyi@users.noreply.github.com>
2025-09-20 23:40:38 +0300

    feat(scripts): macos add system preferences

--- scripts/macos/desktop-env.sh
+++ scripts/macos/desktop-env.sh
@@ -1,18 +1,41 @@
 #!/usr/bin/env bash
 
-setup_settings() {
-  print_in_purple "Applying desktop settings..."
+setup_preferences() {
+  print_in_purple "Applying system preferences..."
   sleep 2
-
-  print_in_green "Desktop settings applied successfully!"
+  defaults write -g InitialKeyRepeat -int 10
+  defaults write -g KeyRepeat -int 1
+  defaults write com.apple.dock "autohide" -bool "true"
+  defaults write com.apple.dock "show-recents" -bool "false"
+  defaults write com.apple.screencapture "disable-shadow" -bool "true"
+  defaults write com.apple.screencapture "location" -string "$HOME/Pictures/Screenshots"
+  defaults write NSGlobalDomain "AppleShowAllExtensions" -bool "true"
+  defaults write com.apple.finder "AppleShowAllFiles" -bool "true"
+  defaults write com.apple.finder "FXPreferredViewStyle" -string "Nlsv"
+  defaults write com.apple.finder "_FXSortFoldersFirst" -bool "true"
+  defaults write com.apple.finder "_FXSortFoldersFirstOnDesktop" -bool "true"
+  defaults write NSGlobalDomain "NSDocumentSaveNewDocumentsToCloud" -bool "false"
+  defaults write com.apple.menuextra.clock "FlashDateSeparators" -bool "true"
+  defaults write com.apple.HIToolbox AppleFnUsageType -int "2"
+  defaults write NSGlobalDomain AppleKeyboardUIMode -int "2"
+  defaults write kCFPreferencesAnyApplication TSMLanguageIndicatorEnabled -bool "false"
+  defaults write com.apple.TextEdit "RichText" -bool "false"
+  defaults write com.apple.CloudSubscriptionFeatures.optIn "545129924" -bool "false"
+  defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool "true"
+  defaults write com.apple.desktopservices DSDontWriteUSBStores -bool "true"
+  chflags nohidden "$HOME/Library"
+  killall Dock
+  killall SystemUIServer
+  killall Finder
+  print_in_green "System preferences applied successfully!"
 }
 
 install_apps() {
-  print_in_purple "Installing desktop apps..."
+  print_in_purple "Installing apps..."
   sleep 2
   local packages="$DIR/packages/apps-brew.txt"
   brew bundle --file="$packages"
-  print_in_green "Desktop apps installed successfully!"
+  print_in_green "Apps installed successfully!"
 }
 
 setup_fonts() {
@@ -41,7 +64,7 @@ setup_terminal() {
 }
 
 desktop_env() {
-  setup_settings
+  setup_preferences
   install_apps
   setup_fonts
   setup_terminal
