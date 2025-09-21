1e9b2c2b3fe75d50ef2e34a46e2f658e6422f0c0
Slava Maksimov <28267670+slavafyi@users.noreply.github.com>
2025-09-21 13:40:01 +0300

    feat(configs): macos change defaults

--- scripts/macos/desktop-env.sh
+++ scripts/macos/desktop-env.sh
@@ -5,27 +5,28 @@ setup_preferences() {
   sleep 2
   local screenshot_folder="$HOME/Pictures/Screenshots"
   mkdir -pv "$screenshot_folder"
+  defaults write -g AppleInterfaceStyleSwitchesAutomatically -bool true
   defaults write -g InitialKeyRepeat -int 15
   defaults write -g KeyRepeat -int 2
-  defaults write com.apple.dock "autohide" -bool "true"
-  defaults write com.apple.dock "show-recents" -bool "false"
-  defaults write com.apple.screencapture "disable-shadow" -bool "true"
-  defaults write com.apple.screencapture "location" -string "$screenshot_folder"
-  defaults write NSGlobalDomain "AppleShowAllExtensions" -bool "true"
-  defaults write com.apple.finder "AppleShowAllFiles" -bool "true"
-  defaults write com.apple.finder "FXPreferredViewStyle" -string "Nlsv"
-  defaults write com.apple.finder "_FXSortFoldersFirst" -bool "true"
-  defaults write com.apple.finder "_FXSortFoldersFirstOnDesktop" -bool "true"
-  defaults write NSGlobalDomain "NSDocumentSaveNewDocumentsToCloud" -bool "false"
-  defaults write com.apple.menuextra.clock "FlashDateSeparators" -bool "true"
-  defaults write com.apple.HIToolbox AppleFnUsageType -int "2"
-  defaults write NSGlobalDomain AppleKeyboardUIMode -int "2"
-  defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool "false"
-  defaults write kCFPreferencesAnyApplication TSMLanguageIndicatorEnabled -bool "false"
-  defaults write com.apple.TextEdit "RichText" -bool "false"
-  defaults write com.apple.CloudSubscriptionFeatures.optIn "545129924" -bool "false"
-  defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool "true"
-  defaults write com.apple.desktopservices DSDontWriteUSBStores -bool "true"
+  defaults write com.apple.dock autohide -bool true
+  defaults write com.apple.dock show-recents -bool false
+  defaults write com.apple.screencapture disable-shadow -bool true
+  defaults write com.apple.screencapture location -string "$screenshot_folder"
+  defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+  defaults write com.apple.finder AppleShowAllFiles -bool true
+  defaults write com.apple.finder FXPreferredViewStyle -string Nlsv
+  defaults write com.apple.finder _FXSortFoldersFirst -bool true
+  defaults write com.apple.finder _FXSortFoldersFirstOnDesktop -bool true
+  defaults write NSGlobalDomain NSDocumentSaveNewDocumentsToCloud -bool false
+  defaults write com.apple.menuextra.clock FlashDateSeparators -bool true
+  defaults write com.apple.HIToolbox AppleFnUsageType -int 2
+  defaults write NSGlobalDomain AppleKeyboardUIMode -int 2
+  defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
+  defaults write kCFPreferencesAnyApplication TSMLanguageIndicatorEnabled -bool false
+  defaults write com.apple.TextEdit RichText -bool false
+  defaults write com.apple.CloudSubscriptionFeatures.optIn 545129924 -bool false
+  defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
+  defaults write com.apple.desktopservices DSDontWriteUSBStores -bool true
   chflags nohidden "$HOME/Library"
   killall Dock
   killall SystemUIServer
