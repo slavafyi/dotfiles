910db22df65d1e042a7a9b8f8e8b61df9782d811
Slava Maksimov <28267670+slavafyi@users.noreply.github.com>
2025-09-22 21:47:36 +0300

    feat(configs): fish add eza

---
+++ configs/fish/.config/fish/conf.d/10-aliases.fish
@@ -0,0 +1,6 @@
+if type -q eza
+  alias ls="eza -1 --group-directories-first"
+  alias la="eza -1a --group-directories-first"
+  alias ll="eza -la --group-directories-first"
+  alias tree="eza -T --group-directories-first"
+end
