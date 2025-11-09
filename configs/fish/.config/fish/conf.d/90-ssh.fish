da473264700009d9926f54cf877ce30a98e38eb4
Slava M. <mailer@noreply.slava.fyi>
2025-11-09 20:13:32 +0300

    feat(configs): add function to update ssh auth socket inside tmux

--- configs/fish/.config/fish/conf.d/90-ssh.fish
+++ configs/fish/.config/fish/conf.d/90-ssh.fish
@@ -1,5 +1,14 @@
+function __tmux_update_ssh_auth_sock --on-event fish_preexec
+    if set -q TMUX
+        set -l new_sock (string replace --regex "^SSH_AUTH_SOCK=" "" \
+                  (tmux show-environment SSH_AUTH_SOCK))
+        if test "$new_sock" != "$SSH_AUTH_SOCK"
+            set -gx SSH_AUTH_SOCK "$new_sock"
+        end
+    end
+end
+
 if set -q SSH_FORWARDING_ENABLED; and test $SSH_FORWARDING_ENABLED -eq 1
-    echo "SSH agent forwarding detected, skipping agent setup"
     exit 0
 end
 
