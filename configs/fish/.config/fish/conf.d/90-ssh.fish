60a97857be3f1d9a3753f081c2cc84bf99d4caef
Slava M. <mailer@noreply.slava.fyi>
2025-11-09 16:02:15 +0300

    feat(configs): add condition to detect ssh-agent forwarding - remove SSH_AUTH_SOCK unnecessary manipulation

--- configs/fish/.config/fish/conf.d/90-ssh.fish
+++ configs/fish/.config/fish/conf.d/90-ssh.fish
@@ -1,19 +1,8 @@
-set os (uname)
+if set -q SSH_FORWARDING_ENABLED; and test $SSH_FORWARDING_ENABLED -eq 1
+    echo "SSH agent forwarding detected, skipping agent setup"
+    exit 0
+end
 
 if type -q ssh-agent
-    if not test "$os" = Darwin
-        if type -q pgrep
-            if not pgrep -u "$USER" ssh-agent >/dev/null
-                rm -v "$SSH_AGENT_SOCK"
-            end
-        end
-
-        if not test -S SSH_AUTH_SOCK
-            eval "$(ssh-agent -c)" >/dev/null
-            set -Ux SSH_AUTH_SOCK "$SSH_AUTH_SOCK"
-            set -Ux SSH_AGENT_PID "$SSH_AGENT_PID"
-        end
-    end
-
     ssh-add ~/.ssh/id_ed25519 2>/dev/null
 end
