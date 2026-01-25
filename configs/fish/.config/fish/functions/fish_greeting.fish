30ad663608e8047c9c77ed8738f0dd0c40759af5
Slava M. <mailer@noreply.slava.fyi>
2026-01-26 02:51:00 +0300

    feat(scripts): zeit remove

--- configs/fish/.config/fish/functions/fish_greeting.fish
+++ configs/fish/.config/fish/functions/fish_greeting.fish
@@ -1,10 +1,2 @@
 function fish_greeting --description "Set fish greeting"
-    if type -q zeit
-        echo
-        set_color brblack
-        echo "○ Time tracking status:"
-        zeit
-        set_color normal
-        echo
-    end
 end
