77f64f2853d849eb9045f6b8c110cf59fc33dd8d
Slava M. <mailer@noreply.slava.fyi>
2026-08-30 02:19:28 +0300

    feat(configs): fish change pi-update func

--- configs/fish/.config/fish/functions/pi-update.fish
+++ configs/fish/.config/fish/functions/pi-update.fish
@@ -6,9 +6,9 @@ function pi-update --description "Update pi-agent and extensions"
     set -l extensions $PI_CODING_AGENT_DIR/extensions
 
     test -f $examples/handoff.ts
-    and test -f $examples/tools.ts
+    and test -f $examples/notify.ts
     or return 1
 
     mkdir -p $extensions
-    command rsync -a $examples/handoff.ts $examples/notify.ts $examples/tools.ts $extensions/
+    command rsync -a $examples/handoff.ts $examples/notify.ts $extensions/
 end
